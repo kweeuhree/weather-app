44ba9c7181d46dbbb13ed49c11243e5a84cfe430
Nika K <firstnamenika@gmail.com>
2024-05-21 20:27:38 -0400

    restyled top segment to display temperature larger

--- src/components/TopSegment/TopSegment.jsx
+++ src/components/TopSegment/TopSegment.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './TopSegmentStyle.css';
+import { WiDegrees } from "react-icons/wi";
 
 const TopSegment = ({ location, units, currentWeather }) => {
   return (
     <div className="top-segment">
 
-    {/* top section left */}
+
+    <div className="top-container">{location.name}</div>
+
+    <div className="middle-container">
+        <div className="temperature-parent">
+
+        <div className="current-condition-icon">
+            <img src={currentWeather.condition.icon} alt={currentWeather.condition.text} />
+        </div>
+       <div className='temperature'> {currentWeather[`feelslike_${units}`]}<span><WiDegrees /></span></div>
+
+
+        </div>
+    </div>
+
+    <div className="bottom-container">{currentWeather.condition.text}</div>
+    {/* top section left
     <div className="top-section left">
         <h3 className="name">{location.name}</h3>
         <div className="temperature">
@@ -14,12 +31,12 @@ const TopSegment = ({ location, units, currentWeather }) => {
         <div className='current-condition'>{currentWeather.condition.text}</div>
     </div>
         
-        {/* top section right */}
+        {/* top section right 
     <div className="top-section right">
     <div className="current-condition-icon">
         <img src={currentWeather.condition.icon} alt={currentWeather.condition.text} />
     </div>
-    </div>
+    </div> */}
 
     </div>
   )
