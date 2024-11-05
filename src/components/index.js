5f50216cba43dfdbaddc2bab3cbf95da3c99c778
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-05 15:59:01 -0500

    feat: create export files in /utils and /components in order to be able to organize imports in components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from './Button/Button';
+export * from './Card/Card';
+export * from './Favorites/Favorites';
+export * from './HomePage/HomePage';
+export * from './Form/Form';
+export * from './TopSegment/TopSegment';
