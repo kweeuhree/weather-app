1ffe5e547c899bd155499001770eed6899d5a025
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-07 09:57:46 -0500

    feat: update Favorites component to add MUI; update message when Favorites is empty

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,4 +1,30 @@
-import { IoHeartOutline } from "react-icons/io5";
+import { useState } from "react";
+
+import { IoHeartOutline, IoHeartSharp  } from "react-icons/io5";
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+
+import './favoritesStyles.css'
+
+const HeartIcon = () => {
+  const [hover, setHover] = useState(false);
+  
+  return (
+    <ListItemIcon 
+    className="display-flex flex-center" 
+    onMouseEnter={() => setHover(true)} 
+    onMouseLeave={() => setHover(false)}
+  >
+   {
+    hover 
+    ? <IoHeartOutline className="red-heart" />
+    : <IoHeartSharp className="red-heart"/>}
+  </ListItemIcon>
+  )
+}
 
 
 export const Favorites = ({ setCurrentCity, units, favoriteCities, toggleFavs }) => {
@@ -8,20 +34,35 @@ const handleClick = (city) => {
   setCurrentCity(city);
 }
 
-  const citiesJSX = favoriteCities.map((city, index) => {
-    return (
-      <div className='fav-location' key={index} >
-          <span className='fav-location-span' onClick={()=> handleClick(city)}>{city.current.location.name} {city.current.current[`feelslike_${units}`]} </span>
-        
-          <span className='heart-span' onClick={(event)=> toggleFavs(city, event)}>
-            <IoHeartOutline />
-          </span>
-    </div>
-    )
-  })
+  const citiesJSX = favoriteCities.map((city, index) => (
+    <ListItem key={index} className='chip'>
+        <div onClick={()=> handleClick(city)} className="display-grid full-width">
+          <div>{city.current.location.name}</div> 
+          <div>{city.current.current[`feelslike_${units}`]}</div>
+        </div>
+      
+        <ListItemButton onClick={(event)=> toggleFavs(city, event)}>
+          <HeartIcon />
+        </ListItemButton>
+    </ListItem>
+    ))
 
   return (
-    <div className='fav-location-container'>{citiesJSX.length > 0 ? citiesJSX : <span>Your Locations</span>}</div>
+    <div className='fav-location-container full-height'>
+      {
+        citiesJSX.length > 0 
+        ? <List 
+            className="full-height display-flex flex-center flex-column gap-1rem">
+              {citiesJSX}
+          </List>
+        : (
+        <div className="display-flex flex-center flex-column pd-1rem white-font full-height">
+          <strong>Your favorite cities will appear here</strong>
+          <p>Click the heart icon to add cities to your favorites list.</p>
+        </div>
+        )
+      }
+    </div>
   )
 }
 
