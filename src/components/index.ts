6f82105e6c79d7141555d3d0128da71dbc5f91a6
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-08 09:38:30 -0500

    feat: update exports from /utils and /components folders

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export * from './Form/Form';
 export * from './Preview/Preview';
 export * from './Details/Details';
 export * from './Loading';
+export * from './HeartIcon';
+export * from './Hourly';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './geolocation';
 export * from './weatherAPIUtils';
 export * from './favoriteCitiesUtils';
-export * from './useFavoriteCities';
 export * from './reducer';
