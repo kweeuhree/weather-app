41c864b53a1430483fb0b2568551fcaeb869b838
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-08 09:36:04 -0500

    feat: add types to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,13 @@
-export const Button = ({ type, onClick, children, ariaLabel }) => {
+import { MouseEventHandler } from "react";
+
+type Props = {
+  type: 'button' | 'submit';
+  children: React.ReactNode;
+  ariaLabel: string;
+  onClick?: MouseEventHandler;
+}
+
+export const Button: React.FC<Props> = ({ type, onClick, children, ariaLabel }) => {
   return (
     <button type={type} aria-label={ariaLabel} onClick={onClick}>{children}</button>
   );
