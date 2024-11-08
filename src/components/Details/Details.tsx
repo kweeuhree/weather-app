77f24b64b3f163263606d67920a95c4b9662210e
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-08 09:35:34 -0500

    feat: add types to Details

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,62 +1,16 @@
-import { useRef } from 'react';
-
 import { GiSunrise, GiSunset  } from "react-icons/gi";
 import { WiHot, WiRaindrops, WiWindy, WiUmbrella } from "react-icons/wi";
 
 import Grid2 from "@mui/material/Grid2";
 
+import { Hourly } from '../index';
+import type { City, DayForecast } from '../../types';
+
 import './detailsStyles.css';
 
-const hourlyJSX = (hour, units) => {
-  const currentTime = new Date().getHours();
-  const displayHours = hour.slice(currentTime);
 
-  const itemRefs = useRef([]);
+const dailyJSX = (day: DayForecast, units: string) => {
 
-  return (
-    displayHours.map(({ condition: { icon, text }}, index) => {
-      const displayHour = currentTime + index;
-
-      const focusRef = (el) => {
-        itemRefs.current[index] = el;
-        // Focus the first element in the array
-        if (index === 0 && el) {
-          el.focus();
-          
-          el.scrollIntoView({
-            behavior: 'smooth', 
-            block: 'center',   
-          });
-        }
-      };
-
-      return (
-        <div key={index} className='full-width'>
-          <li
-            className="display-flex flex-evenly gap-1rem plain-li"
-            ref={focusRef}
-            tabIndex="-1" // Set `tabIndex` so the element can be focused
-          >
-            <div>
-              {displayHour < 10 && '0'}{displayHour}:00 {displayHour < 12 ? 'AM' : 'PM'}
-            </div>
-            
-            <em>
-             {hour[displayHour][`feelslike_${units}`]}
-            </em>
-
-            <div>
-              <img src={icon} alt={text} />
-            </div>
-          </li>
-          <hr />
-        </div>
-      );
-    })
-  );
-};
-
-const dailyJSX = (day, units) => {
   return (
       <div className='display-flex flex-center card pd-1rem'>
         <div>         
@@ -84,14 +38,19 @@ const sunriseSunsetJSX = ({ sunrise, sunset }) => (
 );
 
 
-export const Details = ({ currentCity, units }) => {
-    if(!currentCity.current) return;
+type Props = {
+  currentCity?: City,
+  units: string,
+}
+
+const Details: React.FC<Props> = ({ currentCity, units }) => {
+    if(!currentCity?.current || !currentCity?.forecast) return;
 
     const { current, forecast } = currentCity;
     const { current: currentWeather } = current;
     const { uv, wind_dir, precip_in } = currentWeather;
 
-    const { location, forecast:  { forecastday } } = forecast;
+    const { forecast:  { forecastday } } = forecast;
     const { hour, day, astro } = forecastday[0]; 
 
     const weatherDetails = {
@@ -117,7 +76,9 @@ export const Details = ({ currentCity, units }) => {
     <div className="display-flex flex-center flex-column">
       
       <div className="display-flex flex-center full-width">
-          <ul className="hourly-list display-flex flex-center flex-column full-width">{hourlyJSX(hour, units)}</ul>
+          <ul className="hourly-list display-flex flex-center flex-column full-width">
+            <Hourly hour={hour} units={units} />
+          </ul>
       </div>
 
       <Grid2 container spacing={2} className="display-flex flex-center full-width">
@@ -146,4 +107,4 @@ export const Details = ({ currentCity, units }) => {
   );
 };
 
-
+export default Details;
