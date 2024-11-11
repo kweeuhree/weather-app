c94667a78eee2fcb9cf656de7ebe8e834758f989
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-11 15:26:53 -0500

    feat: disable caching

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port : 3001
+    port : 3001,
+    headers: {
+      'Cache-Control': 'no-store', // Disable caching for dev server
+    },
   },
   build: {
     outDir: 'dist'
