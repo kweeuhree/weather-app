e5746f742e425f8a5c9d136a60e3db35a9460bf2
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-11 15:32:42 -0500

    feat: implement custom useWeatherData hook that leverages useSuspenseQuery for fetching and caching weather data

---
+++ src/hooks/useWeatherData.ts
@@ -0,0 +1,13 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { fetchCoordinates } from '../utils';
+
+/**
+ * This hook uses the `useSuspenseQuery` from React Query.
+ * Fetches weather data based on user location.
+ */
+export function useWeatherData() {
+  return useSuspenseQuery({
+    queryKey: ['weatherData'],  
+    queryFn: () => fetchCoordinates(),     
+})
+}
