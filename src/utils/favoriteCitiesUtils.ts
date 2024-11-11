3e5d4a0c3cdb3c8f00c3f697c89775834f2c2e54
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-11 15:35:14 -0500

    feat: add descriptions to helper functions in favoriteCitiesUtils.ts

--- src/utils/favoriteCitiesUtils.ts
+++ src/utils/favoriteCitiesUtils.ts
@@ -1,12 +1,14 @@
 import type { City } from "../types";
 
+// Checks if a given city is already in the list of favorite cities based on its coordinates (latitude and longitude)
 export const isCityFavorite = (city: City, favoriteCities: City[]) => {
 
     return favoriteCities.some(
       item => item?.current?.location.lat === city.current.location.lat && item?.current?.location.lon === city.current.location.lon
     );
   }
-  
+
+// Removes a specific city from the list of favorite cities based on its coordinates (latitude and longitude)
 export const filterFavoriteCities = (city: City, favoriteCities: City[]) => {
     return favoriteCities.filter(
       (item) =>
