61e1e75d8a2e0d589c547055e9987086b9e67c71
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-11 15:28:56 -0500

    feat: create a fallback component for the App

---
+++ src/components/AppFallback/AppFallback.tsx
@@ -0,0 +1,12 @@
+import './appFallbackStyles.css';
+
+export const AppFallback = () => {
+  return (
+    <div className='spinner pd-1rem'>
+        <h2>🌍 Let Us Show You the Weather Nearby</h2>
+        <p>Please allow location access to get local weather details.</p>
+        <p>Search for specific locations if you'd prefer not to share yours.</p>
+    </div>
+  )
+}
+
