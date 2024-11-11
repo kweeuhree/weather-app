142b99a523eb44ede828815d3a4821b5bb2edbef
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-11 15:28:23 -0500

    feat: add react-query - wrap App in QueryClientProvider; - wrap App in Suspense and add a fallback component

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,36 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 import { App } from './App.tsx';
+import { AppFallback } from './components';
+
 
 import './index.css';
 
 
+const queryClient = new QueryClient();
+
 const rootElement: HTMLElement | null = document.getElementById('root');
+
 
 try {
   if (rootElement) {
     const root = createRoot(rootElement);
     root.render(
-      <Router>
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </Router>
+      <StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Suspense fallback={<div><AppFallback /></div>}>
+                <App />
+            </Suspense>
+          </Router>
+        </QueryClientProvider>
+      </StrictMode>
     );
   }
 } catch (error) {
